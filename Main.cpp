70a7991b1557264e991179e5c92e9abfb6282ced
DayDev <sabhyasoni20@gmail.com>
2023-12-15 16:14:12 +0530

    ain

---
+++ Main.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{   
+    string firstName;
+    cout << "Type your first name: ";
+    cin >> firstName;   
+    if (firstName == firstName) {
+        cout << " Welcome sir";
+    };
+    else
+    {
+      cout << "Sorry"  
+    };
+    
+
+
+
+};
